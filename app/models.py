1b6138a56eae344b9202a20009f8c7f0f0eb53d9
Rajaul <uddin.rajaul1@gmail.com>
2023-04-10 18:05:59 +0545

    commiting

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class JobPost(models.Model):
     description = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
     salary = models.IntegerField()
+    slug = models.SlugField(null=True)
 
     def __str__(self) -> str:
         return self.title
