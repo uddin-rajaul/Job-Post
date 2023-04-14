24c5a9deb7adc156bfae229e9076e91ac7fd26ae
Rajaul <uddin.rajaul1@gmail.com>
2023-04-14 19:29:50 +0545

    customized and added fieldsets, admin panel done

--- app/admin.py
+++ app/admin.py
@@ -7,7 +7,17 @@ class JobAdmin(admin.ModelAdmin):
     list_filter = ('date','salary', 'expiry',)
     search_fields = ('title','description', 'salary',)
     search_help_text = "Write in your Query and hit 'Search'!!!"
-
+    # fields = (('title', 'description'), 'expiry',)
+    # exclude = ('title',)
+    fieldsets = (
+        ('Basic Information', {
+         'fields':('title','description')
+         }),
+         ('More Information', {
+         'classes':('collapse','wide',),
+         'fields':(('expiry','salary'), 'slug')
+         }),
+    )
 
 # Register your models here.
 admin.site.register(JobPost, JobAdmin)
