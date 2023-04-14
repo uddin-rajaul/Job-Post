208c82afde24cec737af8dd2afcee4f6efb8c542
Rajaul <uddin.rajaul1@gmail.com>
2023-04-14 13:51:17 +0545

    Added search in admin panel

--- app/admin.py
+++ app/admin.py
@@ -5,6 +5,8 @@ from app.models import JobPost
 class JobAdmin(admin.ModelAdmin):
     list_display = ('__str__','title', 'date', 'salary',)
     list_filter = ('date','salary', 'expiry',)
+    search_fields = ('title','description', 'salary',)
+    search_help_text = "Write in your Query and hit 'Search'!!!"
 
 
 # Register your models here.
