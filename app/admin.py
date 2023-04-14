e2e4d425e1ae5bdaecdea54cc55daf326f0e6409
Rajaul <uddin.rajaul1@gmail.com>
2023-04-14 13:44:57 +0545

    Filters

--- app/admin.py
+++ app/admin.py
@@ -4,6 +4,7 @@ from app.models import JobPost
 
 class JobAdmin(admin.ModelAdmin):
     list_display = ('__str__','title', 'date', 'salary',)
+    list_filter = ('date','salary', 'expiry',)
 
 
 # Register your models here.
