8fb2d93eba3e5adc9edce373b2737fc2a0fd79d2
Rajaul <uddin.rajaul1@gmail.com>
2023-04-16 20:30:31 +0545

    One to many relation

--- app/models.py
+++ app/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.utils.text import slugify
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    designation = models.CharField(max_length=200)
+
 class Location(models.Model):
     street = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
     state = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
     zip = models.CharField(max_length=200)
+
 
 class JobPost(models.Model):
     title = models.CharField(max_length=200)
@@ -17,6 +24,7 @@ class JobPost(models.Model):
     salary = models.IntegerField()
     slug = models.SlugField(null=True, max_length=30, unique=True)
     location = models.OneToOneField(Location, on_delete=models.CASCADE, null=True) # used to simultaneously delete or update an entry from both the child and parent table
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
 
 
     def save(self, *args, **kwargs):
