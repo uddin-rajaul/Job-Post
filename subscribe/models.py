fde0355cb2ec438485a24bc79ebaa47ffd47ee80
Rajaul <uddin.rajaul1@gmail.com>
2023-05-14 18:50:29 +0545

    Working with choices

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+NEWSLETTER_OPTION   = [
+    ('W', 'Weekly'),
+    ('M', 'Monthly')
+]
+
 # Create your models here.
 class Subscribe(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    option = models.CharField(max_length=2, choices=NEWSLETTER_OPTION, default='M')
+
