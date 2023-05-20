14afb895fba58c8140750a5b7bd0774b4400e39b
Rajaul <uddin.rajaul1@gmail.com>
2023-05-20 13:50:59 +0545

    File upload

--- uploadapp/urls.py
+++ uploadapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('image', views.upload_image, name='upload_image')
+    path('image', views.upload_image, name='upload_image'),
+    path('file', views.upload_file, name='upload_file'),
 ]
--- uploadapp/views.py
+++ uploadapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from uploadapp.forms import UploadForm
+from uploadapp.forms import UploadFileForm, UploadForm
 
 # Create your views here.
 def upload_image(request):
@@ -14,3 +14,15 @@ def upload_image(request):
     else:
         form = UploadForm()
     return render(request, 'uploadapp/add_image.html', {'form':form})
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid:
+            form.save()
+            saved_object=form.instance
+            return render(request, 'uploadapp/add_file.html', {'form':form, 'saved_object':saved_object})
+            
+    else:
+        form = UploadFileForm()
+    return render(request, 'uploadapp/add_file.html', {'form':form})
