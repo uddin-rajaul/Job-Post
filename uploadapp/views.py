6dbffa36c3ea24e9a1a02f875c228032460b15c4
Rajaul <uddin.rajaul1@gmail.com>
2023-05-20 12:55:37 +0545

    saving and rendering image

--- uploadapp/views.py
+++ uploadapp/views.py
@@ -8,6 +8,9 @@ def upload_image(request):
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid:
             form.save()
+            saved_object=form.instance
+            return render(request, 'uploadapp/add_image.html', {'form':form, 'saved_object':saved_object})
+            
     else:
         form = UploadForm()
     return render(request, 'uploadapp/add_image.html', {'form':form})
